cfc21991a8b70a970e94495cf66fbcdbcfce8d56
gustavo <gustavocosta060@gmail.com>
2020-05-19 23:52:05 -0300

    Corrigindo erro

--- Aula07/Erro02.java
+++ Aula07/Erro02.java
@@ -1,10 +1,14 @@
 package Aula07;
+import java.util.Scanner;
 
 public class Erro02 {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		int cont; //variável não iniciada
 		
+		System.out.print("Digite um número: ");
+		cont = s.nextInt();
 		System.out.println("Você digitou: " + cont);
 		
 	}
