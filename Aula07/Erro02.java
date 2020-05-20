edf999c886f808c6f9852392ca0cd3da29c5eb1c
gustavo <gustavocosta060@gmail.com>
2020-05-19 23:48:22 -0300

    Erro 02

---
+++ Aula07/Erro02.java
@@ -0,0 +1,12 @@
+package Aula07;
+
+public class Erro02 {
+
+	public static void main(String[] args) {
+		int cont; //variável não iniciada
+		
+		System.out.println("Você digitou: " + cont);
+		
+	}
+
+}
