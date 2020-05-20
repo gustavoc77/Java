54594501d0c2b1730a85ec9834a8c396bfb389d1
gustavo <gustavocosta060@gmail.com>
2020-05-19 23:45:44 -0300

    Erro 01

---
+++ Aula07/Erro01.java
@@ -0,0 +1,9 @@
+package Aula07;
+
+public class Erro01 {
+	public static void main(String [] args) {
+		//nome da classe não pode ser diferente do nome do arquivo
+		System.out.println("Hello World");
+	}
+	
+}
