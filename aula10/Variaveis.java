325d1433920af886f1b5339920a83ca318a85b2a
Gustavo Costa <47879289+gustavoc77@users.noreply.github.com>
2020-05-21 01:19:02 -0300

    Convenção Java

---
+++ aula10/Variaveis.java
@@ -0,0 +1,34 @@
+package aula10; //pacotes sempre em letra minúscula
+//imports
+
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Gustavo";
+		String nomeCachorro = "Bolt";
+		String nome_cachorro; //não é convenção Java
+		String ano2020 = "2020";
+		
+		//permitido mas não utilizado
+		int _idade; 
+		int $idade; 
+		
+		
+		
+		idade = 25; //trocando valor da idade
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+		
+		//má pratica
+		int a = 10;
+		String b = "Gustavo";
+		
+
+	}
+
+}
