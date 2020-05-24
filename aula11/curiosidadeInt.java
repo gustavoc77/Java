32d98c074a7ab5cf9241521b3bb17e55ae9ab289
gustavo <gustavocosta060@gmail.com>
2020-05-24 01:09:12 -0300

    Tipos Primitivos

---
+++ aula11/curiosidadeInt.java
@@ -0,0 +1,15 @@
+package aula11;
+
+public class curiosidadeInt {
+
+	public static void main(String[] args) {
+		
+		int var1 = 2147483647; //valor máximo de um int
+		
+		int var2 = 100;
+		
+		System.out.println(var1 + var2);
+		
+	}
+
+}
