b29bdf0492429fb0d50b7829191790a8a7e6dcd6
gustavo <gustavocosta060@gmail.com>
2020-05-24 00:45:14 -0300

    Tipos Primitivos

---
+++ Aula11/VariaveisChar.java
@@ -0,0 +1,18 @@
+package Aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		//char o = 'o';
+		//char i = 'i';
+		
+		//utilizando valores da tabela ASCII
+		char o = 111;
+		char i = 105;
+		char interrogacao = 63;
+		
+		System.out.println("" + o+i+interrogacao);
+	}
+
+}
