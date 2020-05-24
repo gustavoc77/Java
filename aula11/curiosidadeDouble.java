fe4b09668a17be2894288f43a525cf5c0a78fd5a
gustavo <gustavocosta060@gmail.com>
2020-05-24 01:14:33 -0300

    Tipos Primitivos

---
+++ aula11/curiosidadeDouble.java
@@ -0,0 +1,21 @@
+package aula11;
+
+public class curiosidadeDouble {
+
+	public static void main(String[] args) {
+
+		double d1= 123.4;
+		
+		//mesmo valor que d1, mas em notação científica
+		double d2 = 1.23e2;
+		
+		float f1 = 123.4f;
+		
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(f1);
+
+		
+	}
+
+}
