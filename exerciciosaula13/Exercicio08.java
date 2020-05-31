eb126dee24d31234db69aa668cd22fb59fe3abf3
gustavo <gustavocosta060@gmail.com>
2020-05-31 18:08:28 -0300

    Exercício 08

--- exerciciosaula13/Exercicio08.java
+++ exerciciosaula13/Exercicio08.java
@@ -11,10 +11,12 @@ public class Exercicio08 {
 		Scanner scan = new Scanner(System.in);
 		double ganhoHora, ganhoTotal;
 		int horasTrabalhadas;
+		
 		System.out.println("Quanto é o ganho por hora? ");
 		ganhoHora = scan.nextDouble();
 		System.out.println("Número de horas trabalhadas no mês: ");
 		horasTrabalhadas = scan.nextInt();
+		
 		ganhoTotal = horasTrabalhadas * ganhoHora;
 		System.out.println("O seu salário total é: R$" + ganhoTotal);
 	}
