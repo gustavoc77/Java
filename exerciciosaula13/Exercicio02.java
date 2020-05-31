166159789d7f3ee609d32cc0456c1ba8d0b493c5
gustavo <gustavocosta060@gmail.com>
2020-05-31 15:40:39 -0300

    Exercício 02

---
+++ exerciciosaula13/Exercicio02.java
@@ -0,0 +1,14 @@
+package exerciciosaula13;
+
+import java.util.Scanner;
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numero;
+		System.out.println("Digite um número: ");
+		numero = scan.nextInt();
+		System.out.println("O número informado foi " + numero );
+	}
+
+}
