fc7253216e9d9b1cd4fd6c9bfe3d958b6245e988
gustavo <gustavocosta060@gmail.com>
2020-05-31 16:16:52 -0300

    Exercicio 04

---
+++ exerciciosaula13/Exercicio04.java
@@ -0,0 +1,21 @@
+/*Faça um Programa que peça as 4 notas bimestrais e mostre a média.*/
+package exerciciosaula13;
+
+import java.util.Scanner;
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		float[] nota = new float[4];
+		float media, soma = 0;
+		int i;
+		for(i=0; i<4; i++) {
+			System.out.println("Digite a " + (i+1) + " nota: ");
+			nota[i] = scan.nextFloat();
+			soma+=nota[i];
+		}
+		media = soma / 4;
+		System.out.println("Média: " + media);
+	}
+
+}
