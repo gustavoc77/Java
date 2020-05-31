39dedbcf318f247d4eb81bd03886a88385ba8467
gustavo <gustavocosta060@gmail.com>
2020-05-31 18:07:22 -0300

    Exercício 08

---
+++ exerciciosaula13/Exercicio08.java
@@ -0,0 +1,21 @@
+/*Faça um Programa que pergunte quanto você ganha por hora e o
+número de horas trabalhadas no mês. Calcule e mostre o total do seu
+salário no referido mês.*/
+package exerciciosaula13;
+
+import java.util.Scanner;
+public class Exercicio08 {
+
+	public static void main(String [] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double ganhoHora, ganhoTotal;
+		int horasTrabalhadas;
+		System.out.println("Quanto é o ganho por hora? ");
+		ganhoHora = scan.nextDouble();
+		System.out.println("Número de horas trabalhadas no mês: ");
+		horasTrabalhadas = scan.nextInt();
+		ganhoTotal = horasTrabalhadas * ganhoHora;
+		System.out.println("O seu salário total é: R$" + ganhoTotal);
+	}
+}
