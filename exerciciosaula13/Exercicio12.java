6d99b17ff1f5cfffac5122733cf901c123518bf9
gustavo <gustavocosta060@gmail.com>
2020-05-31 23:11:39 -0300

    Exercício 13

--- exerciciosaula13/Exercicio12.java
+++ exerciciosaula13/Exercicio12.java
@@ -1,6 +1,3 @@
-/*Tendo como dados de entrada a altura de uma pessoa, construa um
-algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
-(72.7*altura) - 58*/
 package exerciciosaula13;
 
 import java.util.Scanner;
