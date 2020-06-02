d7d25e3f2c58d296e24310dedc20139ea6e5d2d8
gustavo <gustavocosta060@gmail.com>
2020-06-02 00:57:10 -0300

    Operadores Relacionais

---
+++ aula14/IfElse2.java
@@ -0,0 +1,32 @@
+package aula14;
+
+import java.util.Scanner;
+public class IfElse2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		//barato <= 10
+		//10 < valor > 15 - pedir desconto
+		//15 < valor > 17 - pesquisar mais
+		//>= 17 - muito caro
+		
+		System.out.println("Entre com o preço do item: ");
+		double valor = scan.nextDouble();
+		
+		if (valor <= 10) {
+			System.out.println("Está barato, pode comprar");
+		}
+		else if (valor > 10 && valor < 15) {
+			System.out.println("Você pode pedir desconto");
+		}
+		
+		else if (valor >=15 && valor < 17) {
+			System.out.println("Pode pesquisar mais");
+		}
+		else {
+			System.out.println("Muito caro");
+		}
+	}
+
+}
